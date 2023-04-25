5fc4a7571b0fbee0f247f185f9677dfc24c2f94e
filbertfederico <91465463+filbertfederico@users.noreply.github.com>
2023-04-25 23:39:31 +0700

    update
    right click to drop a different circle(for testing the physics)

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import pymunk.pygame_util
 pymunk.pygame_util.positive_y_is_up = False
 
 RES = WIDTH, HEIGHT = 1500, 1000
-FPS = 60
+FPS = 120
 
 pygame.init()
 surface = pygame.display.set_mode(RES)
@@ -14,10 +14,26 @@ space = pymunk.Space()
 space.gravity = 0, 2000
 
 def create_ball(space, pos):
-    ball_mass, ball_radius = 1, 10
+    # mass n radius settings
+    ball_mass, ball_radius = 18.5, 3
+    # inertia
     ball_moment = pymunk.moment_for_circle(ball_mass, 0, ball_radius)
     ball_body = pymunk.Body(ball_mass, ball_moment)
     ball_body.position = pos
+    # phy
+    ball_shape = pymunk.Circle(ball_body, ball_radius)
+    ball_shape.elasticity = 1
+    ball_shape.friction = 0.5
+    space.add(ball_body, ball_shape)
+
+def create_ball1(space, pos):
+    # mass n radius settings
+    ball_mass, ball_radius = 10**30, 20
+    # inertia
+    ball_moment = pymunk.moment_for_circle(ball_mass, 0, ball_radius)
+    ball_body = pymunk.Body(ball_mass, ball_moment)
+    ball_body.position = pos
+    # phy
     ball_shape = pymunk.Circle(ball_body, ball_radius)
     ball_shape.elasticity = 0.8
     ball_shape.friction = 0.5
@@ -51,9 +67,20 @@ while True:
     for i in pygame.event.get():
         if i.type == pygame.QUIT:
             exit()
-        if i.type == pygame.MOUSEBUTTONDOWN:
-            if i.button == 1:
-                create_ball(space, i.pos)
+        elif i.type == pygame.MOUSEBUTTONDOWN and i.button == 3:
+             if i.button == 3:
+                create_ball1(space, i.pos)
+        elif i.type == pygame.MOUSEBUTTONDOWN and i.button == 1:
+                left_mouse_button_down = True
+        elif i.type == pygame.MOUSEBUTTONUP and i.button == 1:
+                left_mouse_button_down = False
+
+    # Get the current state of the mouse buttons
+    mouse_buttons = pygame.mouse.get_pressed()
+
+    # Check if the left mouse button is being held down
+    if mouse_buttons[0] and left_mouse_button_down:
+        create_ball(space, i.pos)
 
     space.step(1/FPS)
     space.debug_draw(draw_options)
